2b5b6eaded59269e1bee4f6f452e351fcb217337
Juan José Chiroque <j.chiroque@uniandes.edu.co>
2022-09-18 18:17:09 -0400

    feat(product): complete controller controller, add product collection

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
 import { ProductDto } from './product.dto';
@@ -22,9 +22,21 @@ export class ProductController {
     }
 
     @Post()
-    async create(@Body() museumDto: ProductDto) {
-        const product: ProductEntity = plainToInstance(ProductEntity, museumDto);
+    async create(@Body() productDto: ProductDto) {
+        const product: ProductEntity = plainToInstance(ProductEntity, productDto);
         return await this.producService.create(product);
+    }
+
+    @Put(':productId')
+    async update(@Param('productId') productId: string, @Body() productDto: ProductDto) {
+        const product: ProductEntity = plainToInstance(ProductEntity, productDto);
+        return await this.producService.update(productId, product);
+    }
+
+    @Delete(':productId')
+    @HttpCode(204)
+    async delete(@Param('productId') productId: string) {
+        return await this.producService.delete(productId);
     }
 
 }
